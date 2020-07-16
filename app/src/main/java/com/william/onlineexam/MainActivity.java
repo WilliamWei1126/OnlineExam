add58c8356da1dc7fa2af6b7315f1f08c8014924
willi <williamwei2004@gmail.com>
2020-07-16 11:24:17 +0800

    Add name

--- app/src/main/java/com/william/onlineexam/MainActivity.java
+++ app/src/main/java/com/william/onlineexam/MainActivity.java
@@ -7,21 +7,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+//Thia ia WIlliam's project
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btn = findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
-                Toast.makeText(MainActivity.this, "I love coding", Toast.LENGTH_SHORT).show();
-
-            }
-        });
+       
     }
 
     }
